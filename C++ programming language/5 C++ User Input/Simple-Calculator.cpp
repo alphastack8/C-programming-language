93625da8ef05be07275d33169ccd0a2fc03b01be
Alpha Stack <alphastack8@gmail.com>
2026-04-13 19:49:07 +0500

    Add files via upload

---
+++ C++ programming language/5 C++ User Input/Simple-Calculator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    int sum = a+b;
+    cout<<"Enter First Number :";
+    cin >> a ;
+    cout<<"Enter Second Number :";
+    cin>> b ;
+    sum = a + b;
+    cout<<"Sum of These Number :"<<sum;
+    return 0;
+
+
+
+}
